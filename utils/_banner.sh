2a1b2a55dc2b698cd01f90d09a9861f59e01718b
Saunier35 <valeriotalk@gmail.com>
2023-03-22 20:50:52 -0300

    first commit

---
+++ utils/_banner.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Print banner art.
+
+#######################################
+# Print a board. 
+# Globals:
+#   BG_BROWN
+#   NC
+#   WHITE
+#   CYAN_LIGHT
+#   RED
+#   BLUE
+#   GREEN
+#   YELLOW
+# Arguments:
+#   None
+#######################################
+print_banner() {
+
+  clear
+
+  printf "\n\n"
+
+  printf "${BLUE}";
+  printf "                               WHATSATENDE TALK - SISTEMA DE MULTIATENDIMENTO                 \n";
+  printf "                                 www.hostshopbr.com | 92 9209-6875                            \n";
+  printf "\n"
+ 
+  printf "${NC}";
+
+  printf "\n"
+}
